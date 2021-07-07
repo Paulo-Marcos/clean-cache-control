3c86863ad85e795b7fc49aaf674bf4243c92ac00
Paulo Marcos <paulo_marcos13@hotmail.com>
2021-07-06 23:25:14 -0300

    feat: add jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  tesEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest' /* qualquer arquivo que termine com .ts vamos transformar isso usando o ts-jest, isto é, para javascript */
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1' /*Onde tiver @/qualquer_coisa será substituído por <rootDir>/src/qualquer_coisa */
+  }
+}
