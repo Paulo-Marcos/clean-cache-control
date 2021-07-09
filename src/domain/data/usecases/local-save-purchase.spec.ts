6ec721aae4af831a7e629d179c1c5a6fd2e1911e
Paulo Marcos <paulo_marcos13@hotmail.com>
2021-07-08 22:37:47 -0300

    feat<localSavePurchase>: Garantir que seja deletado apena o cache informado pela chave

--- src/domain/data/usecases/local-save-purchase.spec.ts
+++ src/domain/data/usecases/local-save-purchase.spec.ts
@@ -2,19 +2,21 @@ class LocalSavePurchases {
   constructor(private readonly cacheStore: CacheStore) { }
 
   async save(): Promise<void> {
-    this.cacheStore.delete()
+    this.cacheStore.delete('purchase')
   }
 }
 
 interface CacheStore {
-  delete: () => void
+  delete: (key: string) => void
 }
 
 class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0
+  key: string
 
-  delete(): void {
+  delete(key: string): void {
     this.deleteCallsCount++
+    this.key = key
   }
 }
 
@@ -44,10 +46,10 @@ describe('LocalSavePurchases', () => {
     expect(cacheStore.deleteCallsCount).toBe(1)
   })
 
-  test('Garantir que o cache antigo seja deletado', async () => {
+  test('Deverá chamar o método delete com a chave correta', async () => {
     const { cacheStore, sut } = makeSut()
     await sut.save()
-    expect(cacheStore.deleteCallsCount).toBe(1)
+    expect(cacheStore.key).toBe('purchase')
   })
 })
 
