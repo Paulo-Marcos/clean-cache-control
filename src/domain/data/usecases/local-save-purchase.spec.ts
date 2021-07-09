aed04d16880d67cfdca34a93def28c3d0c52f7d9
Paulo Marcos <paulo_marcos13@hotmail.com>
2021-07-08 22:29:24 -0300

    Garantir que o cache antigo seja deletado

--- src/domain/data/usecases/local-save-purchase.spec.ts
+++ src/domain/data/usecases/local-save-purchase.spec.ts
@@ -1,18 +1,35 @@
 class LocalSavePurchases {
   constructor(private readonly cacheStore: CacheStore) { }
+
+  async save(): Promise<void> {
+    this.cacheStore.delete()
+  }
 }
 
-interface CacheStore { }
+interface CacheStore {
+  delete: () => void
+}
 
 class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0
+
+  delete(): void {
+    this.deleteCallsCount++
+  }
 }
 
 describe('LocalSavePurchases', () => {
   test('Garantir que o cache não seja apagado ao iniciar', () => {
     const cacheStore = new CacheStoreSpy()
-    const sut = new LocalSavePurchases(cacheStore)
+    new LocalSavePurchases(cacheStore)
     expect(cacheStore.deleteCallsCount).toBe(0)
+  })
+
+  test('Garantir que o cache antigo seja deletado', async () => {
+    const cacheStore = new CacheStoreSpy()
+    const sut = new LocalSavePurchases(cacheStore)
+    await sut.save()
+    expect(cacheStore.deleteCallsCount).toBe(1)
   })
 })
 
