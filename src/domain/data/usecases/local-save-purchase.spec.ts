7737bb081df85ce8b108a5a0dd1720f82fd07b7c
Paulo Marcos <paulo_marcos13@hotmail.com>
2021-07-08 22:34:58 -0300

    feat<localSavePurchase>: mover SUT para uma método de fabricação

--- src/domain/data/usecases/local-save-purchase.spec.ts
+++ src/domain/data/usecases/local-save-purchase.spec.ts
@@ -18,16 +18,34 @@ class CacheStoreSpy implements CacheStore {
   }
 }
 
+type SutTypes = {
+  sut: LocalSavePurchases
+  cacheStore: CacheStoreSpy
+}
+
+const makeSut = (): SutTypes => {
+  const cacheStore = new CacheStoreSpy()
+  const sut = new LocalSavePurchases(cacheStore)
+
+  return {
+    sut, cacheStore
+  }
+}
+
 describe('LocalSavePurchases', () => {
   test('Garantir que o cache não seja apagado ao iniciar', () => {
-    const cacheStore = new CacheStoreSpy()
-    new LocalSavePurchases(cacheStore)
+    const { cacheStore } = makeSut()
     expect(cacheStore.deleteCallsCount).toBe(0)
   })
 
   test('Garantir que o cache antigo seja deletado', async () => {
-    const cacheStore = new CacheStoreSpy()
-    const sut = new LocalSavePurchases(cacheStore)
+    const { cacheStore, sut } = makeSut()
+    await sut.save()
+    expect(cacheStore.deleteCallsCount).toBe(1)
+  })
+
+  test('Garantir que o cache antigo seja deletado', async () => {
+    const { cacheStore, sut } = makeSut()
     await sut.save()
     expect(cacheStore.deleteCallsCount).toBe(1)
   })
